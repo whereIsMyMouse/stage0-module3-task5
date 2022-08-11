bc2a2f11078936ccf9b9a705d994e51d87316002
shariy <sharii.margarita@gmail.com>
2022-08-12 02:06:43 +0300

    commit1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        Double d = new Double(toBeDivided);
+        System.out.println(d/5);
     }
 }
